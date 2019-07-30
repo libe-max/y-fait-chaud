11c4c68876efbb37715713d5a7d96dca864b5cc8
Maxime Fabas <m.fabas@liberation.fr>
2019-07-30 11:49:59 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -17,7 +17,7 @@ module.exports = {
     ? 'https://www.liberation.fr/apps/static'
     : 'http://localhost:3003',
   api_url: process.env.NODE_ENV === 'production'
-    ? 'https://libe-labo.site/api'
+    ? 'https://libe-labo-2.site/api'
     : 'http://localhost:3004/api',
   stylesheet: 'fait-chaud.css',
   spreadsheet: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTATp3BeeWxgyb_Wj-eLN06Mfgqej3LxIGqb1gpfcmCbRp8pb3ht11-uX_oXiYNGH9K_jaGi70vLEHK/pub?gid=0&single=true&output=tsv'
